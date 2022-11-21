9577e946d296b7cb5195b6e40b1f77992bf74ce7
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2022-11-21 11:46:33 -0600

    spell check line 4

--- js/external.js
+++ js/external.js
@@ -1,7 +1,7 @@
 console.log('Hello from external JavaScript');
 alert('Welcome to my website!');
 
-let userInput = prompt('What is your favotire color?');
+let userInput = prompt('What is your favorite color?');
 console.log('Great, ' + userInput + ' is my favorite color too!');
 
 //Exercise 3
