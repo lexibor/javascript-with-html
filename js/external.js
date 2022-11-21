bea66e9439671489fb8626c823e29d970faa15a6
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2022-11-21 11:44:16 -0600

    addes userInput in external js

--- js/external.js
+++ js/external.js
@@ -1,2 +1,7 @@
 console.log('Hello from external JavaScript');
 alert('Welcome to my website!');
+
+let userInput = prompt('What is your favotire color?');
+console.log('Great, ' + userInput + ' is my favorite color too!');
+
+//Exercise 3
