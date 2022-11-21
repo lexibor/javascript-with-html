6fee3e8f4714456bee9597f0d69c5673f0367d96
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2022-11-21 11:53:18 -0600

    random

--- js/external.js
+++ js/external.js
@@ -4,4 +4,4 @@ console.log('Hello from external JavaScript');
 alert('Welcome to my website!');
 
 let userInput = prompt('What is your favorite color?');
-console.log('Great, ' + userInput + ' is my favorite color too!');
+console.log('Great, ' + userInput + ' is my favorite color too!!');
